7cb1b78212d7460de9e6cf064253c624823293df
ilhomjon55 <ilhomshamidinov55@gmail.com>
2020-12-19 22:46:34 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+	if (action === 'danger' || action === 'warning' || action === 'success')
+		return action;
+
+	return state;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [color, setColor] = useReducer(reducer, 'secondary');
+
+	return (
+		<>
+			<div className='container my-5'>
+				<div className='first form-group d-flex align-items-center'>
+					<button
+						className='btn btn-danger'
+						value='danger'
+						onClick={function (evt) {
+							setTimeout(setColor(evt.target.value), 3000);
+						}}>
+						Switch
+					</button>
+					<button
+						className='btn btn-warning mx-auto'
+						value='warning'
+						onClick={function (evt) {
+							setTimeout(setColor(evt.target.value), 3000);
+						}}>
+						Switch
+					</button>
+					<button
+						className='btn btn-success'
+						value='success'
+						onClick={function (evt) {
+							setTimeout(setColor(evt.target.value), 3000);
+						}}>
+						Switch
+					</button>
+				</div>
+				<div className='second form-group d-flex align-items-center'>
+					<div className={`p-5 w-25 mr-2 rounded-lg bg-${color}`}></div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
